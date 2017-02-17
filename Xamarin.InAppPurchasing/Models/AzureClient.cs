5cbda330b9a12f98bd6655032a932ce38b230863
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-02-16 21:10:58 -0600

    Update README.md
    Ported code in unit test to AzureClient

---
+++ Xamarin.InAppPurchasing/Models/AzureClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Xamarin.InAppPurchasing
+{
+    public class AzureClient
+    {
+        private const string BaseUrl = "https://xamarin-iap.azurewebsites.net/api/";
+        private readonly HttpClient _client = new HttpClient();
+
+        public async Task Verify(AppleReceipt receipt)
+        {
+            var content = new JsonContent(receipt);
+            var response = await _client.PostAsync(BaseUrl + "ios?code=KE5uZhyDC6bsu4eJuBMBtPpbBOLE3NYmpDUZOhdQQfCeCoSOn8t8iw==", content);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ Xamarin.InAppPurchasing/Models/JsonContent.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Xamarin.InAppPurchasing
+{
+    public class JsonContent : HttpContent
+    {
+        private static readonly JsonSerializer _serializer = new JsonSerializer();
+        private readonly object _object;
+
+        public JsonContent(object obj)
+        {
+            Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            _object = obj;
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = -1;
+            return false;
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                using (var writer = new StreamWriter(stream, Encoding.UTF8, 4096, true))
+                using (var json = new JsonTextWriter(writer))
+                {
+                    _serializer.Serialize(json, _object);
+                }
+            });
+        }
+    }
+}
