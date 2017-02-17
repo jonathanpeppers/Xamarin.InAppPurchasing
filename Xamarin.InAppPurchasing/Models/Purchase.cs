f1ad1c57a03e2fb15dc5257eba8e900ba3cc4301
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-02-16 21:10:15 -0600

    Shared code for abstract PurchaseService

---
+++ Xamarin.InAppPurchasing/Models/Purchase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Xamarin.InAppPurchasing
+{
+    /// <summary>
+    /// A cross-platform abstraction of an in-app purchase
+    /// </summary>
+    public class Purchase
+    {
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The localized price to display on the UI
+        /// </summary>
+        public string Price { get; set; }
+
+        /// <summary>
+        /// In the case of iOS, an SKPayment object will be placed here
+        /// </summary>
+        public object NativeObject { get; set; }
+    }
+}
---
+++ Xamarin.InAppPurchasing/Models/PurchaseService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Xamarin.InAppPurchasing
+{
+    public abstract class PurchaseService
+    {
+        protected readonly AzureClient _client = new AzureClient();
+
+        /// <summary>
+        /// Retrieves the prices from iTunes or Google Play
+        /// </summary>
+        public abstract Task<Purchase[]> GetPrices(params string[] ids);
+
+        /// <summary>
+        /// Buys an in-app purchase
+        /// </summary>
+        public async Task Buy(Purchase purchase)
+        {
+            var receipt = await BuyNative(purchase);
+
+            var appleReceipt = receipt as AppleReceipt;
+            if (appleReceipt != null)
+            {
+                await _client.Verify(appleReceipt);
+                return;
+            }
+
+            //TODO: Google Play here
+        }
+
+        protected abstract Task<Receipt> BuyNative(Purchase purchase);
+    }
+}
