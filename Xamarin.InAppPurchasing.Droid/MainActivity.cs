518169a4197fc7f07ae82b46bef1c23e9c63ec0b
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-03-09 15:04:48 -0600

    File | New - Android project

---
+++ Xamarin.InAppPurchasing.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Xamarin.InAppPurchasing.Droid
+{
+    [Activity(Label = "Xamarin.InAppPurchasing.Droid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
