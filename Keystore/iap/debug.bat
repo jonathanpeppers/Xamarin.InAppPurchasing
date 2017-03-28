d8d1a70fd90bc5c58185df2bbb1ed5407d2a0a54
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-03-28 16:43:41 -0500

    Cleanup batch script

--- Keystore/iap/debug.bat
+++ Keystore/iap/debug.bat
@@ -1,7 +1,10 @@
 @echo off
 set KEYTOOL="C:\Program Files (x86)\Java\jdk1.8.0_101\bin\keytool.exe"
+set KEYSTORE="iap.keystore"
+set ALIAS="iap"
+set PASSWORD="YOUR_KEYSTORE_PASSWORD"
 
-copy /y iap.keystore debug.keystore
-%KEYTOOL% -changealias -keystore debug.keystore -alias iap -destalias androiddebugkey
-%KEYTOOL% -keypasswd -keystore debug.keystore -alias androiddebugkey -new android
-%KEYTOOL% -storepasswd -keystore debug.keystore -new android
+copy /y %KEYSTORE% debug.keystore
+%KEYTOOL% -changealias -keystore debug.keystore -alias %ALIAS% -destalias androiddebugkey -storepass %PASSWORD%
+%KEYTOOL% -keypasswd -keystore debug.keystore -alias androiddebugkey -new android -storepass %PASSWORD%
+%KEYTOOL% -storepasswd -keystore debug.keystore -new android -storepass %PASSWORD%
