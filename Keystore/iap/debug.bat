9385928c1a21a4bf81f0099af1574f1cc175d972
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-03-28 16:52:46 -0500

    More batch file cleanup

--- Keystore/iap/debug.bat
+++ Keystore/iap/debug.bat
@@ -1,10 +1,17 @@
 @echo off
+
+rem Change these variables for your keystore
 set KEYTOOL="C:\Program Files (x86)\Java\jdk1.8.0_101\bin\keytool.exe"
 set KEYSTORE="iap.keystore"
 set ALIAS="iap"
 set PASSWORD="YOUR_KEYSTORE_PASSWORD"
 
-copy /y %KEYSTORE% debug.keystore
-%KEYTOOL% -changealias -keystore debug.keystore -alias %ALIAS% -destalias androiddebugkey -storepass %PASSWORD%
-%KEYTOOL% -keypasswd -keystore debug.keystore -alias androiddebugkey -new android -storepass %PASSWORD%
-%KEYTOOL% -storepasswd -keystore debug.keystore -new android -storepass %PASSWORD%
+rem These are variables that Xamarin.Android expects
+set OUT_KEYSTORE="debug.keystore"
+set OUT_ALIAS="androiddebugkey"
+set OUT_PASSWORD="android"
+
+copy /y %KEYSTORE% %OUT_KEYSTORE%
+%KEYTOOL% -changealias -keystore %OUT_KEYSTORE% -alias %ALIAS% -destalias %OUT_ALIAS% -storepass %PASSWORD%
+%KEYTOOL% -keypasswd -keystore %OUT_KEYSTORE% -alias %OUT_ALIAS% -new %OUT_PASSWORD% -storepass %PASSWORD%
+%KEYTOOL% -storepasswd -keystore %OUT_KEYSTORE% -new %OUT_PASSWORD% -storepass %PASSWORD%
